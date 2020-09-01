7b38bf895a8cb3819f509ec6938a5d0777e79ca9
jarbasal <jarbasai@mailfence.com>
2020-09-01 09:08:18 +0100

    #0

--- examples/export_collection.py
+++ examples/export_collection.py
@@ -1,6 +1,8 @@
-from pyvod.collections import Cinemocracy, SciFiHorror
+from pyvod.collections import Cinemocracy, SciFiHorror, FeatureFilms, FeatureFilmsPicfixer
 
 # You can play the generated .m3u8 file in VLC or other player
 
 Cinemocracy.dump_m3u8("Cinemocracy.m3u8")
 SciFiHorror.dump_m3u8("ScifiHorror.m3u8")
+FeatureFilms.dump_m3u8("FeatureFilms.m3u8")
+FeatureFilmsPicfixer.dump_m3u8("FeatureFilmsPicfixer.m3u8")
